12505d277dd0c7c47c300ad169add188a28739a3
artursou <93260734+artursou@users.noreply.github.com>
2025-08-20 15:38:39 +0000

    changes

--- dnc-arq-artur/src/components/Footer/Footer.jsx
+++ dnc-arq-artur/src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 
 
+
 // ASSETS
 import "./Footer.css"
 import Logo from "../../assets/dnc-logo.svg"
@@ -18,9 +19,9 @@ function Footer () {
             <div className="container">
                 <div className="d-flex jc-space-between">
                     <div className="footer-logo-col">
-                        <img src={Logo}></img>
+                        <img src={Logo} />
                         <p className="grey-1-color">A escola que prepara você para as profissões em alta no mercado de trabalho</p>
-                        <div className="d-flex social-link">
+                        <div className="d-flex social-link jc-space-between">
                             <a href="https://google.com" target="_blank">
                                 <img src={FacebookLogo}></img>
                             </a>
@@ -34,6 +35,24 @@ function Footer () {
                                 <img src={InstagramLogo}></img>
                             </a>
                         </div>
+                    </div>
+                    <div className="d-flex">
+                            <div className="footer-col">
+                                <h3>Pages</h3>
+                                <ul className="d-flex">
+                                    <li><Link to={"/"}>Home</Link></li>
+                                    <li><Link to={"/about"}>About</Link></li>
+                                    <li><Link to={"/projects"}>Projects</Link></li>
+                                    <li><Link to={"/contact"}>Contact</Link></li>
+                                </ul>
+                            </div>
+                        </div>
+                </div>
+                <div className="d-flex jc-space-between footer-copy">
+                    <p className="grey-1-color">Copyright DNC - 2024</p>
+                    <div className="langs-area d-flex">
+                        <img src={BrazilLogo} />
+                        <img src={UsaLogo} />
                     </div>
                 </div>
             </div>
